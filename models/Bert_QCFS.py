1d85d924e936d3de86955b841c7ab63f905d944d
Tuan Mai <tuanmai1509work@gmail.com>
2025-09-17 15:14:48 +0700

    fix

--- models/Bert_QCFS.py
+++ models/Bert_QCFS.py
@@ -42,9 +42,18 @@ class BertLayerQCFS(nn.Module):
         self.intermediate = BertIntermediateQCFS(config, T)
         self.output = BertOutputQCFS(config)
 
-    def forward(self, hidden_states, *args, **kwargs):
+    def forward(self, hidden_states, attention_mask=None, head_mask=None, encoder_hidden_states=None, encoder_attention_mask=None, past_key_value=None, output_attentions=False, use_cache=False):
         # Pass all arguments to attention layer
-        attention_output = self.attention(hidden_states, *args, **kwargs)[0]
+        attention_output = self.attention(
+            hidden_states, 
+            attention_mask=attention_mask,
+            head_mask=head_mask,
+            encoder_hidden_states=encoder_hidden_states,
+            encoder_attention_mask=encoder_attention_mask,
+            past_key_value=past_key_value,
+            output_attentions=output_attentions,
+            use_cache=use_cache
+        )[0]
         intermediate_output = self.intermediate(attention_output)
         layer_output = self.output(intermediate_output, attention_output)
         return layer_output
